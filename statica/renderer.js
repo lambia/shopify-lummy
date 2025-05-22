31f186d2b07368ad2714b56dd28039a9f5034825
Luca Lambia <luca@lambia.it>
2025-05-22 21:34:25 +0200

    let's start

--- statica/renderer.js
+++ statica/renderer.js
@@ -1,7 +1,48 @@
-const partialURL = "https://raw.githubusercontent.com/lambia/shopify-lummy/refs/heads/main/statica/partial.html";
+const appWrapper = "appWrapper";
+let gfxCounter = 0;
+const texts = {
+    loading: "Il configuratore è in caricamento",
+    errorLoading: "Si è verificato un errore irreversibile"
+};
 
-fetch(partialURL).then(r => r.text()).then(response => {
-    document.getElementById("conf_preview_columns").innerHTML = response;
-}).catch(err=> {
-    console.error("Impossibile caricare il configuratore", err);
-});
+document.getElementById(appWrapper).innerHTML = texts.loading;
+main();
+
+async function main() {
+    const partial = await getPartial();
+    document.getElementById(appWrapper).innerHTML = "";
+
+    render(partial, gfxCounter);
+    gfxCounter++;
+    render(partial, gfxCounter);
+}
+
+function render(partial="Errore", id=0) {
+
+    const newGfx = `<div class="gfxWrapper" data-gfx-id="${id}">${partial}<div>`;
+
+    document.getElementById(appWrapper).innerHTML += newGfx;
+}
+
+async function getPartial() {
+    const partialURL = "https://raw.githubusercontent.com/lambia/shopify-lummy/refs/heads/main/statica/partial.html";
+    let result = "";
+    
+    try {
+        let partialResponse = await fetch(partialURL);
+        if (!partialResponse.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        let partialText = await partialResponse.text();
+        result = partialText;
+    } catch (error) {
+        console.error("Impossibile caricare il configuratore", error);
+        result = texts.errorLoading;
+    }
+
+    return result;
+}
+
+function init() {
+    console.log("Inizializzato");
+}
---
+++ todo.md
@@ -0,0 +1,4 @@
+Sistemare Disclaimer-Checkbox (non deve abilitare il pulsante se non era già abilitato)
+Creare uno scope per ogni form
+Bottone "aggiungi form"
+Bottone "aggiungi carrello" -> pesca tutti gli scope
