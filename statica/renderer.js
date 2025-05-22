5815bd593888724aae8c2e4b29284b6fa3697c92
Luca Lambia <luca@lambia.it>
2025-05-22 20:52:08 +0200

    external renderer ok

--- statica/renderer.js
+++ statica/renderer.js
@@ -1,8 +1,6 @@
-//const partialURL = "https://www.lambia.it/shopify/partial.txt";
-const partialURL = "https://raw.githubusercontent.com/lambia/shopify-lummy/refs/heads/main/statica/partial.txt";
+const partialURL = "https://raw.githubusercontent.com/lambia/shopify-lummy/refs/heads/main/statica/partial.html";
 
 fetch(partialURL).then(r => r.text()).then(response => {
-    console.log("Response: ", response);
     document.getElementById("conf_preview_columns").innerHTML = response;
 }).catch(err=> {
     console.error("Impossibile caricare il configuratore", err);
